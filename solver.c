406e577d28bc26b12c2c50f4d67d132636fc234e
Franco <fgozzerino@gmail.com>
2025-04-19 16:23:50 -0300

    Intento de vectorizar advect con intrinsics

--- solver.c
+++ solver.c
@@ -136,7 +136,112 @@ static void diffuse(unsigned int n, boundary b, float* x, const float* x0, float
     float a = dt * diff * n * n;
     lin_solve(n, b, x, x0, a, 1 + 4 * a);
 }
+/*
+#include <immintrin.h>
 
+static void advect(unsigned int n, boundary b, float* d, const float* d0, const float* u, const float* v, float dt)
+{
+    const __m256 half = _mm256_set1_ps(0.5f);
+    const __m256 one = _mm256_set1_ps(1.0f);
+    const __m256 n_plus_half = _mm256_set1_ps(n + 0.5f);
+    const __m256 dt0_vec = _mm256_set1_ps(dt * n);
+
+    // Process 8 elements at a time
+    for (unsigned int i = 1; i <= n; i++) {
+        for (unsigned int j = 1; j <= n; j += 8) {
+            // Calculate how many elements to process (might be less than 8 at boundaries)
+            unsigned int count = (i + 8 <= n + 1) ? 8 : (n + 1 - i);
+
+            // Load indices
+            __m256i idx = _mm256_setr_epi32(i, i + 1, i + 2, i + 3, i + 4, i + 5, i + 6, i + 7);
+            __m256i j_vec = _mm256_set1_epi32(j);
+
+            // Load u and v values
+            float u_arr[8] = { 0 }, v_arr[8] = { 0 };
+            for (unsigned int k = 0; k < count; k++) {
+                u_arr[k] = u[IX(i + k, j)];
+                v_arr[k] = v[IX(i + k, j)];
+            }
+            __m256 u_val = _mm256_loadu_ps(u_arr);
+            __m256 v_val = _mm256_loadu_ps(v_arr);
+
+            // Calculate x and y positions
+            __m256 i_vec = _mm256_cvtepi32_ps(idx);
+            __m256 j_vec_ps = _mm256_cvtepi32_ps(j_vec);
+
+            __m256 x = _mm256_sub_ps(i_vec, _mm256_mul_ps(dt0_vec, u_val));
+            __m256 y = _mm256_sub_ps(j_vec_ps, _mm256_mul_ps(dt0_vec, v_val));
+
+            // Clamp x and y values
+            x = _mm256_max_ps(x, half);
+            x = _mm256_min_ps(x, n_plus_half);
+            y = _mm256_max_ps(y, half);
+            y = _mm256_min_ps(y, n_plus_half);
+
+            // Calculate i0, i1, j0, j1
+            __m256 i0_ps = _mm256_floor_ps(x);
+            __m256 i1_ps = _mm256_add_ps(i0_ps, one);
+            __m256 j0_ps = _mm256_floor_ps(y);
+            __m256 j1_ps = _mm256_add_ps(j0_ps, one);
+
+            // Convert to integers for indexing
+            __m256i i0 = _mm256_cvttps_epi32(i0_ps);
+            __m256i i1 = _mm256_cvttps_epi32(i1_ps);
+            __m256i j0 = _mm256_cvttps_epi32(j0_ps);
+            __m256i j1 = _mm256_cvttps_epi32(j1_ps);
+
+            // Calculate interpolation weights
+            __m256 s1 = _mm256_sub_ps(x, i0_ps);
+            __m256 s0 = _mm256_sub_ps(one, s1);
+            __m256 t1 = _mm256_sub_ps(y, j0_ps);
+            __m256 t0 = _mm256_sub_ps(one, t1);
+
+            // Gather required d0 values
+            float d00_arr[8] = { 0 }, d01_arr[8] = { 0 }, d10_arr[8] = { 0 }, d11_arr[8] = { 0 };
+
+            // Extract indices to array
+            int i0_arr[8], i1_arr[8], j0_arr[8], j1_arr[8];
+            _mm256_storeu_si256((__m256i*)i0_arr, i0);
+            _mm256_storeu_si256((__m256i*)i1_arr, i1);
+            _mm256_storeu_si256((__m256i*)j0_arr, j0);
+            _mm256_storeu_si256((__m256i*)j1_arr, j1);
+
+            // Load data manually
+            for (unsigned int k = 0; k < count; k++) {
+                d00_arr[k] = d0[IX(i0_arr[k], j0_arr[k])];
+                d01_arr[k] = d0[IX(i0_arr[k], j1_arr[k])];
+                d10_arr[k] = d0[IX(i1_arr[k], j0_arr[k])];
+                d11_arr[k] = d0[IX(i1_arr[k], j1_arr[k])];
+            }
+
+            __m256 d00 = _mm256_loadu_ps(d00_arr);
+            __m256 d01 = _mm256_loadu_ps(d01_arr);
+            __m256 d10 = _mm256_loadu_ps(d10_arr);
+            __m256 d11 = _mm256_loadu_ps(d11_arr);
+
+            // Perform interpolation
+            __m256 t0_d00 = _mm256_mul_ps(t0, d00);
+            __m256 t1_d01 = _mm256_mul_ps(t1, d01);
+            __m256 t0_d10 = _mm256_mul_ps(t0, d10);
+            __m256 t1_d11 = _mm256_mul_ps(t1, d11);
+
+            __m256 s0_part = _mm256_mul_ps(s0, _mm256_add_ps(t0_d00, t1_d01));
+            __m256 s1_part = _mm256_mul_ps(s1, _mm256_add_ps(t0_d10, t1_d11));
+
+            __m256 result = _mm256_add_ps(s0_part, s1_part);
+
+            // Store results
+            float result_arr[8];
+            _mm256_storeu_ps(result_arr, result);
+
+            for (unsigned int k = 0; k < count; k++) {
+                d[IX(i + k, j)] = result_arr[k];
+            }
+        }
+    }
+    set_bnd(n, b, d);
+}
+*/
 
 static void advect(unsigned int n, boundary b, float* d, const float* d0, const float* u, const float* v, float dt)
 {
@@ -171,6 +276,7 @@ static void advect(unsigned int n, boundary b, float* d, const float* d0, const 
     }
     set_bnd(n, b, d);
 }
+
 
 static void project(unsigned int n, float* u, float* v, float* p, float* div)
 {
