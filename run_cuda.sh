1c3d7bcc835cea530b89b6c47fcd405e4c7177ba
franco <fgozzerino@gmail.com>
2025-06-10 18:40:49 -0300

    update run_cuda2

--- run_cuda.sh
+++ run_cuda.sh
@@ -2,6 +2,8 @@ nvcc -arch=sm_86 -c headless.cu -o headless.o
 
 nvcc -arch=sm_86 -c wtime.cpp -o wtime.o
 
-nvcc -arch=sm_86 solver_rb_cuda.cu_nvcc_-O2.o wtime.o headless.o -o cuda
+nvcc -arch=sm_86 -c solver_rb_cuda.cu -o solver_rb_cuda.o
+
+nvcc -arch=sm_86 solver_rb_cuda.o wtime.o headless.o -o cuda
 
 ./cuda
